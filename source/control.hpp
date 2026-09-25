e67faa8d70f31ce1dd01a5012165778e1f0a1ab4
luis-rochoso <sousaluislfs@gmail.com>
2026-09-25 14:54:59 -0300

    [ADD] Shards and button toggles

--- source/control.hpp
+++ source/control.hpp
@@ -3,20 +3,31 @@
 #include "states.hpp"
 #include "interface.hpp"
 
+Vector2 mousePoint { 0.0f, 0.0f };
+
+bool shardPower [2] = {false, false};
+
 std::unordered_map<std::string, Texture2D> textures;
 
 void init() {
-    static const int SCREEN_WIDTH = 800;
-    static const int SCREEN_HEIGHT = 450;
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Controle da Escotilha");
 
     SetTargetFPS(60);
 
-    textures["shard"] = LoadTexture("./assets/dragonshard.png");
+    Image shard = LoadImage("./assets/dragonshard.png");
+    ImageResize(&shard, 100, 100);
+
+    SetWindowIcon(shard);
+
+    textures["shard"] = LoadTextureFromImage(shard);
+
+    UnloadImage(shard);
 }
 
 void update(Gamestate &state) {
+
+    mousePoint = GetMousePosition();
 
     switch (state)
     {
@@ -25,7 +36,19 @@ void update(Gamestate &state) {
         break;
     
     case open:
-        
+        // Handling pressing Crystal buttons
+        for (int i = 0; i < 2; ++i) {
+            if (CheckCollisionPointCircle(mousePoint, shardButtons[i].center, buttonRadius)) {
+                if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+                    shardPower[i] = !shardPower[i];
+                }
+            }    
+        }
+
+        // Lighting up or out the toggable shards  
+        for (int i = 0; i < 2; ++i) {
+            shardLight[i] = shardPower[i];
+        }
         break;
 
     case dragging:
@@ -48,6 +71,8 @@ void render(Gamestate &state) {
     case open:
         drawBackground();
         drawOpenPlate();
+        drawCrystals(textures["shard"]);
+        drawButtons();
         break;
 
     case dragging:
--- source/interface.hpp
+++ source/interface.hpp
@@ -1,19 +1,70 @@
 #include "raylib.h"
 
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 450;
+
+const float PLATE_X = SCREEN_WIDTH / 16;
+const float PLATE_Y = SCREEN_HEIGHT / 10;
+const float PLATE_WIDTH = PLATE_X * 14;
+const float PLATE_HEIGHT = PLATE_Y * 8;
+
+const float buttonRadius = 25;
+
+struct Circle {
+    Vector2 center;
+    float radius;
+};
+
+// 50 is half the shard texture's height
+Vector2 buttonCenter[2] = {{PLATE_X - (PLATE_WIDTH/16), PLATE_Y + 50},
+                           {PLATE_X - (PLATE_WIDTH/16), PLATE_Y + (PLATE_HEIGHT/3) + 50}};
+
+Circle shardButtons[2] = {{buttonCenter[0], buttonRadius},
+                          {buttonCenter[1], buttonRadius}};
+
+bool shardLight[2] = {false, false};
+
 void drawBackground() {
     ClearBackground(GRAY);
 }
 
 void drawOpenPlate() {
 
-    int plateXStart = GetScreenWidth() / 16;
-    int plateYStart = GetScreenHeight() / 10;
-    int plateWidth = plateXStart * 14;
-    int plateHeight = plateYStart * 8;
-
-    Rectangle plate {plateXStart, plateYStart, plateWidth, plateHeight};
+    Rectangle plate {PLATE_X, PLATE_Y, PLATE_WIDTH, PLATE_HEIGHT};
 
     DrawRectangle(plate.x, plate.y, plate.width, plate.height, BLACK);
     DrawRectangleLinesEx(plate, 10, DARKGRAY);
 
+}
+
+void drawCrystals(Texture2D shard) {
+
+    // Crystal 1 (switchable)
+    if (shardLight[0]) {
+        DrawTexture(shard, PLATE_X, PLATE_Y, RAYWHITE);
+    }
+    else {
+        DrawTexture(shard, PLATE_X, PLATE_Y, GRAY);
+    }
+
+    // Crystal 2 (switchable)
+    if (shardLight[1]) {
+        DrawTexture(shard, PLATE_X, PLATE_Y + (PLATE_HEIGHT/3), RAYWHITE);
+    }
+    else {
+        DrawTexture(shard, PLATE_X, PLATE_Y + (PLATE_HEIGHT/3), GRAY);
+    }
+
+    // Crystal 3 (always on)
+    DrawTexture(shard, PLATE_X, PLATE_Y + ((PLATE_HEIGHT/3) * 2), RAYWHITE);
+
+}
+
+void drawButtons() {
+
+    // Button 1
+    DrawCircle(buttonCenter[0].x, buttonCenter[0].y, buttonRadius, GREEN);
+
+    // Button 2
+    DrawCircle(buttonCenter[1].x, buttonCenter[1].y, buttonRadius, GREEN);
 }
